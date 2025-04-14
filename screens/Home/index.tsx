948b6f105fc5b7d10bff0707a2b8d3482cab696c
Fipera <filipeg.bello@gmail.com>
2025-04-14 01:00:24 -0300

    signin e login

--- screens/Home/index.tsx
+++ screens/Home/index.tsx
@@ -5,10 +5,14 @@ import { Image } from "@/components/ui/image";
 import { Text } from "@/components/ui/text";
 import { VStack } from "@/components/ui/vstack";
 import { House, User } from "lucide-react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+
+import { useRouter } from 'expo-router';
 
 export default () => {
+    const router = useRouter()
     return (
-        <VStack className="flex-1 items-center justify-between px-4 py-6">
+        <VStack className="flex-1 items-center justify-between px-4 py-12">
             <VStack className="items-center">
                 <Image
                     size="lg2"
@@ -32,10 +36,10 @@ export default () => {
                     Como você quer fazer a diferença hoje?
                 </Text>
 
-                <HStack className="w-full justify-center mt-6 gap-4">
+                <HStack className="items-center justify-center mt-6 gap-4 w-full">
                     <Button
                         variant="outline"
-                        className="flex-1 min-w-[140px] max-w-[200px] h-12 bg-button-bg rounded-[8px] shadow-shadow flex-row items-center justify-center"
+                        className="flex-1 min-w-[140px] max-w-[200px] h-12 bg-button-bg rounded-[8px] shadow-shadow flex-row items-center justify-center  data-[active=true]:bg-primary-100"
                     >
                         <HStack className="items-center justify-center gap-2">
                             <ButtonIcon
@@ -50,7 +54,7 @@ export default () => {
 
                     <Button
                         variant="outline"
-                        className="flex-1 min-w-[140px] max-w-[200px] h-12 bg-transparent rounded-[8px] shadow-shadow flex-row items-center justify-center"
+                        className="flex-1 min-w-[140px] max-w-[200px] h-12 bg-transparent rounded-[8px] shadow-shadow flex-row items-center justify-center  data-[active=true]:bg-primary-100"
                     >
                         <ButtonIcon as={User} className="w-6 h-6 text-black" />
                         <ButtonText className="text-primary-600 text-base">
@@ -59,19 +63,20 @@ export default () => {
                     </Button>
                 </HStack>
 
-                <Button variant="link" size="lg" className="mt-4">
+                <Button variant="link" size="lg" className="mt-4" onPress={() => router.push("/signin")}>
                     <ButtonText className="text-black font-roboto text-md font-light">
                         Já tem uma conta? Faça login
                     </ButtonText>
                 </Button>
             </VStack>
 
-            <HStack className="items-center justify-center mt-10 mb-8">
-                <Icon as={User} className="text-black " />
+            <HStack className="items-center justify-center space-x-2">
+            <MaterialIcons name="favorite" size={24} color="#2D543F" />
                 <Text size="sm" className="font-NunitoItalic text-black">
                     Transforme empatia em ação. Faça parte.
                 </Text>
             </HStack>
+
         </VStack>
     );
 };
